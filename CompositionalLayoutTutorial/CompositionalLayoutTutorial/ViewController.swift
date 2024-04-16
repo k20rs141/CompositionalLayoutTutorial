7a857ac7c4f2182a4e5a3b6a2d7afa4e82dec8d1
Yamada Koki <koki.yamada@link-u.co.jp>
2024-04-16 14:49:13 +0900

    Create: New Project

---
+++ CompositionalLayoutTutorial/CompositionalLayoutTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayoutTutorial
+//
+//  Created by k-yamada on 2024/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
