8b61ee44a58ca22f67c872510e1fc5b530786b6c
Yamada Koki <koki.yamada@link-u.co.jp>
2024-04-22 13:23:42 +0900

    Create: db.jsonをルーティングするためのindex.jsを作成

---
+++ index.js
@@ -0,0 +1,38 @@
+// Mock用のJSONレスポンスサーバーの初期化設定
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+
+// Database構築用のJSONファイル
+const router = jsonServer.router('Datasource/db.json');
+
+// 各種設定用
+const middlewares = jsonServer.defaults();
+const rewrite_rules = jsonServer.rewriter({
+    "/api/mock/v1/gourmet/articles" : "/get_articles",
+    "/api/mock/v1/gourmet/keywords" : "/get_keywords",
+    "/api/mock/v1/gourmet/new_arrivals" : "/get_new_arrivals",
+    "/api/mock/v1/gourmet/featured_banners" : "/get_featured_banners",
+    "/api/mock/v1/gourmet/featured_interviews" : "/get_featured_interviews",
+});
+
+// リクエストのルールを設定する
+server.use(rewrite_rules);
+
+// ミドルウェアを設定する (※コンソール出力するロガーやキャッシュの設定等)
+server.use(middlewares);
+
+// 受信したリクエストにおいてGET送信時のみ許可する
+server.use(function (req, res, next) {
+    if (req.method === 'GET') {
+        next();
+    }
+});
+
+// ルーティングを設定する
+server.use(router);
+
+// サーバをポート3000で起動する
+server.listen(3000, () => {
+    console.log('JSON Server is running');
+});
+
