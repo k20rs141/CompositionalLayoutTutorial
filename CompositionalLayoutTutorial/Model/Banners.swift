43ced5f35241a4595078f198c2c5c76353be8ba2
Yamada Koki <koki.yamada@link-u.co.jp>
2024-04-22 13:02:37 +0900

    Create: JsonをCodableするためのモデルを作成

---
+++ CompositionalLayoutTutorial/Model/Banners.swift
@@ -0,0 +1,8 @@
+//
+//  Banner.swift
+//  CompositionalLayoutTutorial
+//
+//  Created by k-yamada on 2024/04/22.
+//
+
+import Foundation
---
+++ CompositionalLayoutTutorial/Model/Interviews.swift
@@ -0,0 +1,12 @@
+//
+//  InterView.swift
+//  CompositionalLayoutTutorial
+//
+//  Created by k-yamada on 2024/04/22.
+//
+
+import Foundation
+
+struct Interviews: Codable {
+    var 
+}
