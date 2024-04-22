3c9df8a14731ff4a6ded9f84bdb2cb0d657aadc8
Yamada Koki <koki.yamada@link-u.co.jp>
2024-04-22 14:15:42 +0900

    Add: promisesライブラリとAPI通信に関連するファイルの作成

---
+++ CompositionalLayoutTutorial/API/APIProvider.swift
@@ -0,0 +1,8 @@
+//
+//  APIProvider.swift
+//  CompositionalLayoutTutorial
+//
+//  Created by k-yamada on 2024/04/22.
+//
+
+import Foundation
---
+++ CompositionalLayoutTutorial/API/APIType.swift
@@ -0,0 +1,8 @@
+//
+//  APIType.swift
+//  CompositionalLayoutTutorial
+//
+//  Created by k-yamada on 2024/04/22.
+//
+
+import Foundation
